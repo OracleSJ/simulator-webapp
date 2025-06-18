88d46d9d13a80799d9249e6b2e7ef6ee15b9f5cc
nedrazzz <smg9972@gmail.com>
2025-06-18 23:21:03 +0900

    chore: add `~` path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,11 +11,16 @@ export default defineConfig({
   },
   plugins: [
     tanstackRouter({
-      target: 'react',
-      autoCodeSplitting: true,      // 자동 코드 분할 활성화(선택)
-      verboseFileRoutes: false      // 파일 라우트 로그(선택)
+      target: "react",
+      autoCodeSplitting: true, // 자동 코드 분할 활성화(선택)
+      verboseFileRoutes: false, // 파일 라우트 로그(선택)
     }),
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
-})
+  resolve: {
+    alias: {
+      "~": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
